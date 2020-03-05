56acb04712679eaab75a5ad020b7154d99be1c17
Malin Hanak <malinehanak@gmail.com>
2020-03-05 13:34:54 +0100

    Complete memory game

--- index.js
+++ index.js
@@ -20,46 +20,51 @@ class MemoryGame {
   flipFirstCard(card) {
     this.flippedCard = true;
     this.firstCard = card;
+    card.style.pointerEvents = 'none';
   }
 
   flipSecondCard(card) {
     this.secondCard = card;
     this.flippedCard = false;
+    card.style.pointerEvents = 'none';
     return this.checkForMatch();
   }
 
   checkForMatch() {
     this.secondCard.dataset.image === this.firstCard.dataset.image
       ? this.disableCards()
-      : this.unflipCards();
+      : this.unFlipCards();
   }
 
-  unflipCards() {
+  unFlipCards() {
     this.lockBoard = true;
     setTimeout(() => {
       this.firstCard.classList.remove('flip');
       this.secondCard.classList.remove('flip');
+      this.firstCard.style.pointerEvents = 'auto';
+      this.secondCard.style.pointerEvents = 'auto';
       this.lockBoard = false;
       this.resetBoard();
     }, 1500);
   }
 
-  unflipAllCards(cards) {
+  unFlipAllCards(cards) {
     this.resetBoard();
     cards.forEach((card) => card.classList.remove('flip'));
   }
 
   disableCards() {
     this.countMatches = this.countMatches + 1;
-    // this.firstCard.removeEventListener('click', this.flipCard, false);
-    // this.secondCard.removeEventListener('click', this.flipCard, false);
+    memory.getCards().forEach((card) => {
+      if (card.className.includes('flip')) {
+        card.style.pointerEvents = 'none';
+      }
+    });
     this.resetBoard();
   }
 
   resetBoard() {
-    console.log('match count', this.countMatches);
     if (this.countMatches === 6) return this.winner();
-    removeListeners();
     this.flippedCard = false;
     this.lockBoard = false;
     this.firstCard = undefined;
@@ -67,10 +72,23 @@ class MemoryGame {
   }
 
   shuffle(cards) {
-    document.getElementById('winner').innerText = '';
-    this.countMatches = 0;
-    this.unflipAllCards(cards);
-    const areas = [
+    let randomPosition;
+    if (this.countMatches > 0 && this.firstCard !== undefined) {
+      this.resetWinner();
+    }
+    this.unFlipAllCards(cards);
+    const areas = this.gameBoardPositions();
+
+    cards.forEach((card) => {
+      randomPosition = areas[Math.floor(Math.random() * areas.length)];
+      card.style.gridArea = randomPosition;
+
+      this.removedUsedPosition(areas, randomPosition);
+    });
+  }
+
+  gameBoardPositions() {
+    return [
       'one',
       'two',
       'three',
@@ -84,48 +102,60 @@ class MemoryGame {
       'eleven',
       'twelve',
     ];
-    let randomPos;
-    cards.forEach((card) => {
-      randomPos = areas[Math.floor(Math.random() * areas.length)];
-      card.style.gridArea = randomPos;
-      for (let i = 0; i < areas.length; i++) {
-        if (areas[i] === randomPos) {
-          areas.splice(i, 1);
-        }
+  }
+
+  removedUsedPosition(areas, position) {
+    for (let i = 0; i < areas.length; i++) {
+      if (areas[i] === position) {
+        areas.splice(i, 1);
       }
-    });
+    }
   }
 
   winner() {
-    document.getElementById('winner').innerText = 'YOU WON!';
+    return this.createAndDisplayWinnerContainer();
+  }
+
+  resetWinner() {
+    this.countMatches = 0;
+    const body = document.querySelector('body');
+    const winner = document.getElementById('winner');
+    body.removeChild(winner);
+  }
+
+  getCards() {
+    return document.querySelectorAll('.card');
+  }
+
+  getWinnerContainer() {
+    return document.getElementById('winner');
+  }
+
+  createAndDisplayWinnerContainer() {
+    const main = document.getElementById('start');
+    const winnerContainer = document.createElement('section');
+    const winnerContent = document.createElement('article');
+    winnerContent.textContent = 'YOU WON!';
+    winnerContainer.appendChild(winnerContent);
+    winnerContainer.setAttribute('id', 'winner');
+    winnerContent.setAttribute('class', 'you-won');
+    document.body.insertBefore(winnerContainer, main);
+  }
+
+  getShuffleButton() {
+    return document.getElementById('shuffle');
   }
 }
 
 const memory = new MemoryGame();
-const cards = document.querySelectorAll('.card');
-document
-  .getElementById('shuffle')
-  .addEventListener('click', () => memory.shuffle(cards));
 
-window.onload = memory.shuffle(cards);
-function flipCard(card) {
-  return memory.flipCard(card);
-}
-function getCards() {
-  return document.querySelectorAll('.card');
-}
-function removeListeners() {
-  return getCards().forEach((card) => {
-    console.log(card.className);
-    console.log('I remove event');
-    if (card.className.includes('flip')) {
-      return card.removeEventListener('click', () => flipCard(card));
-    }
-  });
-}
+memory
+  .getShuffleButton()
+  .addEventListener('click', () => memory.shuffle(memory.getCards()));
+
 window.onload = () => {
-  return cards.forEach((card) => {
-    console.log('I add event');
-    card.addEventListener('click', () => flipCard(card));
+  memory.shuffle(memory.getCards());
+  return memory.getCards().forEach((card) => {
+    card.addEventListener('click', () => memory.flipCard(card), false);
   });
 };
